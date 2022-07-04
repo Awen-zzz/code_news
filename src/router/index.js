45bd4863d23042689591d2650697254638eb34e3
wenyan <1049705898@qq.com>
2022-07-04 23:14:40 +0800

    项目结构初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+// 脚手架生成后就有这文件,vue-router 是vue.js 官方给出的路由解决方案。它只能结合 vue 项目进行使用,能够轻松的管理 SPA (单页面应用)项目中组件的切换。
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
